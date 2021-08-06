42919f5793d21540fb296af3342e93bd2c27aa99
brlaney42 <brlaney@outlook.com>
2021-08-05 19:58:25 -0500

    Currently fixing charts pages

---
+++ src/lib/types/index.ts
@@ -0,0 +1,15 @@
+export interface IBar {
+  country: string
+  hotdog: number
+  hotdogColor: string
+  burger: number
+  burgerColor: string
+  sandwich:number
+  sandwichColor: string
+  kebab:number
+  kebabColor: string
+  fries:number
+  friesColor: string
+  donut:number
+  donutColor: string
+}
---
+++ src/pages/charts/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import styles from '@/styles/Home.module.scss'
+import Charts from '@/components/Charts'
+
+const Home = () => (
+  <>
+    <div id='container' className='uk-container'>
+      <div className='main'>
+        <Charts />
+      </div>
+    </div>
+  </>
+)
+
+export default Home
